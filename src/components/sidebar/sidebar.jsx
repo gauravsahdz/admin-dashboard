fad80afcaa6aaef66258cee51c3e4267dfc3ec31
kuwarpj <kuwarjha2500@gmail.com>
2023-10-17 15:04:18 +0545

    sidebar done

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,113 +1,131 @@
-import React, { useState } from "react";
-import { BrowserRouter as NavLink } from "react-router-dom";
+
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 import { AnimatePresence, motion } from "framer-motion";
+import SidebarMenu from "./sidebarMenu";
 import VpnKeyOutlinedIcon from "@mui/icons-material/VpnKeyOutlined";
 import WidgetsOutlinedIcon from "@mui/icons-material/WidgetsOutlined";
 import AccountBoxOutlinedIcon from "@mui/icons-material/AccountBoxOutlined";
 import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 import InsightsOutlinedIcon from "@mui/icons-material/InsightsOutlined";
 import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlined";
-
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import "./Sidebar.css";
-import SidebarMenu from "./sidebarMenu";
 
 const routes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: <VpnKeyOutlinedIcon />,
-    exact: true,
-  },
-  {
-    name: "Product",
-    icon: <WidgetsOutlinedIcon />,
-    exact: true,
-    subRoutes: [
-      {
-        name: "Add Product",
-        path: "/product/add",
-        exact: true,
-      },
-      {
-        name: "List Product",
-        path: "/product/list",
-        exact: true,
-      },
-    ],
-  },
-  {
-    name: "Customers",
-    icon: <AccountBoxOutlinedIcon />,
-    exact: true,
-    subRoutes: [
-      {
-        name: "Add Customer",
-        path: "/customers/add",
-        exact: true,
-      },
-      {
-        name: "List Customer",
-        path: "/customers/list",
-        exact: true,
-      },
-    ],
-  },
-  {
-    name: "Income",
-    icon: <AccountBalanceWalletOutlinedIcon />,
-    exact: true,
-    subRoutes: [
-      {
-        name: "Add Income",
-        path: "/income/add",
-        exact: true,
-      },
-      {
-        name: "List Income",
-        path: "/income/list",
-        exact: true,
-      },
-    ],
-  },
-  {
-    name: "Promote",
-    icon: <InsightsOutlinedIcon />,
-    exact: true,
-    subRoutes: [
-      {
-        name: "Add Promote",
-        path: "/promote/add",
-        exact: true,
-      },
-      {
-        name: "List Promote",
-        path: "/promote/list",
-        exact: true,
-      },
-    ],
-  },
-  {
-    name: "Help",
-    icon: <ContactSupportOutlinedIcon />,
-    exact: true,
-    subRoutes: [
-      {
-        name: "Add Help",
-        path: "/help/add",
-        exact: true,
-      },
-      {
-        name: "List Help",
-        path: "/help/list",
-        exact: true,
-      },
-    ],
-  },
-];
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      icon: <VpnKeyOutlinedIcon  />,
+      exact: true,
+    },
+    {
+      name: "Product",
+      icon: <WidgetsOutlinedIcon />,
+      exact: true,
+      subRoutes: [
+        {
+          name: "Add Product",
+          path: "/product/add",
+          exact: true,
+        },
+        {
+          name: "List Product",
+          path: "/product/list",
+          exact: true,
+        },
+      ],
+    },
+    {
+      name: "Customers",
+      icon: <AccountBoxOutlinedIcon />,
+      exact: true,
+      subRoutes: [
+        {
+          name: "Add Customer",
+          path: "/customers/add",
+          exact: true,
+        },
+        {
+          name: "List Customer",
+          path: "/customers/list",
+          exact: true,
+        },
+      ],
+    },
+    {
+      name: "Income",
+      icon: <AccountBalanceWalletOutlinedIcon />,
+      exact: true,
+      subRoutes: [
+        {
+          name: "Add Income",
+          path: "/income/add",
+          exact: true,
+        },
+        {
+          name: "List Income",
+          path: "/income/list",
+          exact: true,
+        },
+      ],
+    },
+    {
+      name: "Promote",
+      icon: <InsightsOutlinedIcon />,
+      exact: true,
+      subRoutes: [
+        {
+          name: "Add Promote",
+          path: "/promote/add",
+          exact: true,
+        },
+        {
+          name: "List Promote",
+          path: "/promote/list",
+          exact: true,
+        },
+      ],
+    },
+    {
+      name: "Help",
+      icon: <ContactSupportOutlinedIcon />,
+      exact: true,
+      subRoutes: [
+        {
+          name: "Add Help",
+          path: "/help/add",
+          exact: true,
+        },
+        {
+          name: "List Help",
+          path: "/help/list",
+          exact: true,
+        },
+      ],
+    },
+  ];
 
-const Sidebar = ({ children, isOpen }) => {
+const SideBar = ({ children, isOpen, handleHeaderText }) => {
   const [prevMenuOpen, setPrevMenuOpen] = useState(null);
+
+  const inputAnimation = {
+    hidden: {
+      width: 0,
+      padding: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      width: "150px",
+      padding: "5px 15px",
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
 
   const showAnimation = {
     hidden: {
@@ -126,6 +144,10 @@ const Sidebar = ({ children, isOpen }) => {
     },
   };
 
+  const onItemClick = (menuName, subMenuName) => {
+    handleHeaderText(menuName, subMenuName);
+  };
+
   const handleMenuToggle = (route) => {
     if (prevMenuOpen === route) {
       setPrevMenuOpen(null); // If clicked menu is the same as the open menu, close it
@@ -133,24 +155,32 @@ const Sidebar = ({ children, isOpen }) => {
       setPrevMenuOpen(route); // Otherwise, set the clicked menu as the new open menu
     }
   };
+
+  // const getCurrentUser = () => {
+  //   const user = JSON.parse(localStorage.getItem("user"));
+  //   return user;
+  // };
+
   return (
     <div className="sidebar-main-container">
       <div className="main-container">
+        
         <motion.div
           animate={{
             width: isOpen ? "100%" : "45px",
+
             transition: {
               duration: 0.5,
               type: "spring",
               damping: 10,
             },
           }}
-          className={`sidebar`}
+          className={`sidebar `}
         >
           <div className="top_section">
             <AnimatePresence>
               {isOpen && (
-                <motion.h1
+                <motion
                   variants={showAnimation}
                   initial="hidden"
                   animate="show"
@@ -158,12 +188,14 @@ const Sidebar = ({ children, isOpen }) => {
                   className="logo"
                 >
                   <div className="hospital-logo">
-                    <h3>Dashboard</h3>
+                 <p><AddCircleOutlineIcon  /></p>   <p>Dashboard</p>
+                    
                   </div>
-                </motion.h1>
+                </motion>
               )}
             </AnimatePresence>
           </div>
+         
           <section className="routes">
             {routes.map((route, index) => {
               if (route.subRoutes) {
@@ -173,6 +205,7 @@ const Sidebar = ({ children, isOpen }) => {
                     showAnimation={showAnimation}
                     isOpen={isOpen}
                     key={index}
+                    handleHeaderText={handleHeaderText}
                     isMenuOpened={prevMenuOpen === route}
                     onMenuToggle={() => handleMenuToggle(route)}
                   />
@@ -185,37 +218,56 @@ const Sidebar = ({ children, isOpen }) => {
                   key={index}
                   className="link"
                   activeclassname="active"
+                  onClick={() => onItemClick(route.name, "Home")}
                 >
-                  <div className="menu1">
-                    <div className="icon">{route.icon}</div>
-                    <AnimatePresence>
-                      {isOpen && (
-                        <motion.div
-                          variants={showAnimation}
-                          initial="hidden"
-                          animate="show"
-                          exit="hidden"
-                          className="link_text"
-                        >
-                          {route.name}
-                        </motion.div>
-                      )}
-                    </AnimatePresence>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                  </div>
+                  <div className="icon">{route.icon}</div>
+                  <AnimatePresence>
+                    {isOpen && (
+                      <motion.div
+                        variants={showAnimation}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="link_text"
+                      >
+                        {route.name}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
                 </NavLink>
               );
             })}
+
+
           </section>
+
+        <div className="profile-container" >
+          <div className="profile-wrap">
+        <div className="profile-pic">
+                  <img
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    alt="profile"
+                  />
+                  
+                </div>
+
+                <div className="profile-details">
+                  <p>Kuwar Jha</p>
+                  <p>Project Manager</p>
+                </div>
+                </div>
+        </div>
         </motion.div>
 
+        
+       
+
         <main className="main-cls">{children}</main>
+
+        
       </div>
     </div>
   );
 };
-export default Sidebar;
+
+export default SideBar;
